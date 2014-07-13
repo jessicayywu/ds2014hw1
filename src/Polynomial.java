a6f12a7a8dedc9b237cc4440a1d7df7c1354bda0
Yen-Yi Wu <jessicayywu@yahoo.com.tw>
2014-07-13 16:15:28 +0800

    3. Add add and subtract methods
    ...

--- src/Polynomial.java
+++ src/Polynomial.java
@@ -1,6 +1,7 @@
 public class Polynomial {
-	private Node firstNode, lastNode;
-	private String name;
+	Node firstNode;
+	Node lastNode;
+	String name;
 
 	public Polynomial() { 
 		this("polynomial"); 
@@ -12,28 +13,42 @@ public class Polynomial {
 	}
 	
 	public void insert(int newCoef, int newExp) {
+		if (newCoef == 0)
+			return;
+		
 		if (isEmpty()) {
 			firstNode = lastNode = new Node(newCoef, newExp);
 		}
 		else if (firstNode.exp > newExp) {
 			firstNode = new Node(newCoef, newExp, firstNode);
 		}
-		else if(lastNode.exp < newExp)
+		else if (lastNode.exp < newExp) 
 			lastNode = lastNode.nextNode = new Node(newCoef, newExp);
 		else {
 			Node current = firstNode;
+			Node previous = null;
 
 			while (current != null) {
-				if(current.exp == newExp) {
-					current.coef += newCoef;
-					break;
+				if (current.exp == newExp) {
+					if (current.coef == -newCoef) {
+						if (current == lastNode) 
+							lastNode = previous;
+
+						current = current.nextNode;
+						previous.nextNode = current;
+					}
+					else 
+						current.coef += newCoef;
+					
+					break;					
 				}
-				if(current.exp < newExp && current.nextNode.exp > newExp) {
+				if (current.exp < newExp && current.nextNode.exp > newExp) {
 					Node temp = current.nextNode;
 				    current.nextNode = new Node(newCoef, newExp);
 				    (current.nextNode).nextNode = temp;
 				    break;
 				}
+				previous = current;
 				current = current.nextNode;
 			}
 		}
@@ -41,6 +56,52 @@ public class Polynomial {
 
 	public boolean isEmpty() { 
 		return firstNode == null;
+	}
+	
+	public Polynomial add(Polynomial poly) {
+		Polynomial result = new Polynomial(name + "+" + poly.name);
+		
+		Node current1 = firstNode;
+		Node current2 = poly.firstNode;
+		
+		while (current1 != null) {
+			result.insert(current1.coef, current1.exp);
+			result.print();
+			current1 = current1.nextNode;
+		}
+		
+
+		
+		while (current2 != null) {
+			result.insert(current2.coef, current2.exp);
+			result.print();
+			current2 = current2.nextNode;
+		}
+		
+		return result;
+	}
+	
+	public Polynomial subtract(Polynomial poly) {
+		Polynomial result = new Polynomial(name + "-" + poly.name);
+		
+		Node current1 = firstNode;
+		Node current2 = poly.firstNode;
+		
+		while (current1 != null) {
+			result.insert(current1.coef, current1.exp);
+			result.print();
+			current1 = current1.nextNode;
+		}
+		
+
+		
+		while (current2 != null) {
+			result.insert(-current2.coef, current2.exp);
+			result.print();
+			current2 = current2.nextNode;
+		}
+		
+		return result;
 	}
 
 	public void print() {
@@ -53,8 +114,7 @@ public class Polynomial {
 		Node current = firstNode;
 
 		while (current != null) {
-			if(current.coef != 0)
-				System.out.printf("%+d x^%d ", current.coef, current.exp);
+			System.out.printf("%+d x^%d ", current.coef, current.exp);
 			current = current.nextNode;
 		}
 		System.out.println();
--- src/Test.java
+++ src/Test.java
@@ -1,7 +1,7 @@
 
 public class Test {
 	public static void main(String args[]) {
-		Polynomial poly1 = new Polynomial();
+		Polynomial poly1 = new Polynomial("A");
 		poly1.insert(1, 2);
 		poly1.print();		
 		poly1.insert(2, 0);
@@ -14,7 +14,30 @@ public class Test {
 		poly1.print();
 		poly1.insert(8, 2);
 		poly1.print();
+		poly1.insert(7, 4);
+		poly1.print();
 		
-		Polynomial poly2 = new Polynomial();
+		System.out.println();
+		
+		Polynomial poly2 = new Polynomial("B");
+		poly2.insert(7, 2);
+		poly2.print();
+		poly2.insert(4, 3);
+		poly2.print();
+		poly2.insert(-9, 6);
+		poly2.print();
+		poly2.insert(-8, 5);
+		poly2.print();
+		
+		System.out.println();
+		
+		
+		Polynomial result = poly1.add(poly2);
+		result.print();
+		
+		System.out.println();
+		
+		result = poly1.subtract(poly2);
+		result.print();
 	}
 }
